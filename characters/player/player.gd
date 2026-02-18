f65283cfd4fd07be5d86999b480c8ddf5dc9bd53
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2026-02-18 16:25:32 -0300

    feat: ink, teste de vó

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -6,5 +6,16 @@ var direction: Vector2 = Vector2(1, 1)
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
+	
+	if(velocity.y > 0):
+		$AnimatedSprite2D.play("vo_down")
+	elif(velocity.y < 0):
+		$AnimatedSprite2D.play("vo_up")
+	
+	if(velocity.x > 0):
+		$AnimatedSprite2D.flip_h = 1
+	elif(velocity.x < 0):
+		$AnimatedSprite2D.flip_h = 0
+	
 	move_and_slide()
 	
