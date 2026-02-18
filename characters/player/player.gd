9f5ff6f7a6517d77ace4e8c1c7c7ff932db1fb1b
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2026-02-18 20:26:36 -0300

    teste ink, spritesheet player inicial left down

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -2,20 +2,29 @@ extends CharacterBody2D
 
 const SPEED: float = 250.0
 var direction: Vector2 = Vector2(1, 1)
+var last_direction = 'down'
 
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
 	
 	if(velocity.y > 0):
-		$AnimatedSprite2D.play("vo_down")
+		$AnimatedSprite2D.play("player_down")
+		last_direction = 'down'
 	elif(velocity.y < 0):
-		$AnimatedSprite2D.play("vo_up")
+		# $AnimatedSprite2D.play("vo_up")
+		last_direction = 'up'
 	
 	if(velocity.x > 0):
-		$AnimatedSprite2D.flip_h = 1
+		# $AnimatedSprite2D.play("player_left")
+		last_direction = 'right'
 	elif(velocity.x < 0):
-		$AnimatedSprite2D.flip_h = 0
+		$AnimatedSprite2D.play("player_left")
+		# $AnimatedSprite2D.flip_h = 1
+		last_direction = 'left'
+	elif(velocity.y == 0 && velocity.x == 0):
+		if(last_direction != 'right' && last_direction != 'up'):
+			$AnimatedSprite2D.play('player_idle_'+last_direction)
 	
 	move_and_slide()
 	
---
+++ ink/teste_vo.ink
@@ -0,0 +1,11 @@
+# Lena
+Oi meu querido
+Ó, se seu tio te pedir cerveja, não pegue não viu!
+Ele precisa parar de beber, tá demais já.
+
+# Benicio
++ [Não pegar cerveja agora.]
+    Tá bom vó, pode deixar!
+* [Pegar cerveja.]
+    Ok vó, vou só pegar água na geladeira.
+
