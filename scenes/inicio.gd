d787a6f19403582c858b41d9ce656d20f79c2c6b
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2026-02-28 17:29:28 -0300

    pause menu style

--- scenes/inicio.gd
+++ scenes/inicio.gd
@@ -7,7 +7,7 @@ func _on_jogar_button_pressed():
 	Transicao.transicionar("res://scenes/sala_de_estar_cutscene_1.tscn")
 
 func _on_opcoes_button_pressed():
-	pass # Replace with function body.
+	PauseMenu.pause()
 
 func _on_sair_button_pressed():
 	get_tree().quit()
