dd0cbf0fbc61edc2260be6db067fc2b8efcb27f0
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2026-02-28 16:14:23 -0300

    menu inicial

---
+++ scenes/inicio.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func _ready():
+	pass
+
+func _on_jogar_button_pressed():
+	Transicao.transicionar("res://scenes/sala_de_estar_cutscene_1.tscn")
+
+func _on_opcoes_button_pressed():
+	pass # Replace with function body.
+
+func _on_sair_button_pressed():
+	get_tree().quit()
